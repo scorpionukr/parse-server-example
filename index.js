85b605637c1db6f73d1115f0eed5c4ff0a5ae970
Scorpion <scorpionukr@users.noreply.github.com>
2017-01-12 14:13:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://admin:lakers1234@ds161028.mlab.com:61028/wnd-parse-new',
+  databaseURI: databaseUri || 'mongodb://admin:lakers1234@ds145405.mlab.com:45405/weightsndates-dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || '7IfmJE8zVqi6WkLgdku2wiw2JdaBa6qyBaExhTvt',
   masterKey: process.env.MASTER_KEY || 'yFDKPty9Eob0j1jP1tf7Ln3ISnWP4pCI7G0MBcmh', //Add your master key here. Keep it secret!
   fileKey: process.env.FILE_KEY || '86f11687-2383-4c75-8206-944901d1946d',
-  serverURL: process.env.SERVER_URL || 'https://wnd-parse-new.herokuapp.com:1337/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'https://weightsndates-server-dev.herokuapp.com:1337/parse',  // Don't forget to change to https if needed
   push: {
       android: {
         senderId: '620420937756',
