40a0694dbf5c64b0a7ce5a73defbff145a2f7f7d
pzverkov <peter.zverkov888@gmail.com>
2017-01-18 22:19:02 +0100

    not reject unauth

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://admin:lakers1234@ds145405.mlab.com:45405/weightsndates-dev',
   cloud: __dirname + '/cloud/main.js',
