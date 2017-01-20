8ceaafa44ab5e6cc47658b486c7492741223b7f1
pzverkov <peter.zverkov888@gmail.com>
2017-01-20 09:32:13 +0100

    http

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.get('/test', function (req, res) {
 
 
 var port = process.env.PORT || 1337;
-var httpServer = require('https').createServer(app);
+var httpServer = require('http').createServer(app);
 httpServer.listen(port, function () {
     console.log('parse-server-example running on port ' + port + '.');
 });
