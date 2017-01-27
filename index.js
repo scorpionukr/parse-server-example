270ff19900f57f386bb782dd860fe8c91faa5bc5
Scorpion <scorpionukr@users.noreply.github.com>
2017-01-27 16:11:16 +0200

    delete filekey

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ var api = new ParseServer({
   appId: process.env.APP_ID || '7IfmJE8zVqi6WkLgdku2wiw2JdaBa6qyBaExhTvt',
   applicationId: process.env.APP_ID || '7IfmJE8zVqi6WkLgdku2wiw2JdaBa6qyBaExhTvt',
   masterKey: process.env.MASTER_KEY || 'yFDKPty9Eob0j1jP1tf7Ln3ISnWP4pCI7G0MBcmh', //Add your master key here. Keep it secret!
-  fileKey: process.env.FILE_KEY || '86f11687-2383-4c75-8206-944901d1946d',
   serverURL: process.env.SERVER_URL || 'https://wnd-parse-files.herokuapp.com:1337/parse',  // Don't forget to change to https if needed
   push: {
       android: {
