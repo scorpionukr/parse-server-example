c70c2f9fd586e7e07c4739ac1e1e96ecf9c1d7b9
pzverkov <peter.zverkov888@gmail.com>
2017-01-28 18:34:16 +0100

    format

--- index.js
+++ index.js
@@ -19,48 +19,51 @@ var sender = new gcm.Sender(SERVER_KEY_FCM);
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
+    console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://admin:lakers1234@ds145405.mlab.com:45405/weightsndates-dev',
+    databaseURI: databaseUri || 'mongodb://admin:lakers1234@ds145405.mlab.com:45405/weightsndates-dev',
 
-  //new databaseURI: databaseUri || 'mongodb://admin:lakers1234@ds161028.mlab.com:61028/wnd-parse-new',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || '7IfmJE8zVqi6WkLgdku2wiw2JdaBa6qyBaExhTvt',
-  applicationId: process.env.APP_ID || '7IfmJE8zVqi6WkLgdku2wiw2JdaBa6qyBaExhTvt',
-  masterKey: process.env.MASTER_KEY || 'yFDKPty9Eob0j1jP1tf7Ln3ISnWP4pCI7G0MBcmh', //Add your master key here. Keep it secret!
-  fileKey: '86f11687-2383-4c75-8206-944901d1946d',
-  serverURL: process.env.SERVER_URL || 'https://weightsndates-server-dev.herokuapp.com:1337/parse',  // Don't forget to change to https if needed
-  push: {
-      android: {
-        senderId: '620420937756',
-        apiKey: 'AIzaSyDoTGDyXFzwdkNP09N7_aN7VUerbmxYwbE'
-      },
-      ios: [
-        {
-          pfx: 'wnd.p12', // Dev PFX or P12
-          passphrase: 'kosmos1960',
-          bundleId: 'com.wnd',
-          production: false // Dev
+    //new databaseURI: databaseUri || 'mongodb://admin:lakers1234@ds161028.mlab.com:61028/wnd-parse-new',
+    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    appId: process.env.APP_ID || '7IfmJE8zVqi6WkLgdku2wiw2JdaBa6qyBaExhTvt',
+    applicationId: process.env.APP_ID || '7IfmJE8zVqi6WkLgdku2wiw2JdaBa6qyBaExhTvt',
+    masterKey: process.env.MASTER_KEY || 'yFDKPty9Eob0j1jP1tf7Ln3ISnWP4pCI7G0MBcmh', //Add your master key here. Keep it secret!
+    fileKey: '86f11687-2383-4c75-8206-944901d1946d',
+    serverURL: process.env.SERVER_URL || 'https://weightsndates-server-dev.herokuapp.com:1337/parse',  // Don't forget to change to https if needed
+
+    push: {
+
+        android: {
+            senderId: '620420937756',
+            apiKey: 'AIzaSyDoTGDyXFzwdkNP09N7_aN7VUerbmxYwbE'
         },
-        {
-          pfx: 'apns_prod.p12', // Prod PFX or P12
-          passphrase: 'kosmos1960',
-          bundleId: 'com.wnd',  
-          production: true // Prod
-        }
-      ]
-  },
-  filesAdapter: new S3Adapter(
-    "AKIAIJRIGNXA2BOCWIGA",
-    "IfTGFjY2XtAKe8mopz7Bbfk30YtlF8cnlXGhD1ub",
-    "wnd-files-test",
-    {directAccess: true}
-  ),
-  liveQuery: {
-    classNames: ["Posts", "Comments", "Conversation", "Message", "Fcm"] // List of classes to support for query subscriptions
-  }
+
+        ios: [
+            {
+                pfx: 'wnd.p12', // Dev PFX or P12
+                passphrase: 'kosmos1960',
+                bundleId: 'com.wnd',
+                production: false // Dev
+            },
+            {
+                pfx: 'apns_prod.p12', // Prod PFX or P12
+                passphrase: 'kosmos1960',
+                bundleId: 'com.wnd',
+                production: true // Prod
+            }
+        ]
+    },
+    filesAdapter: new S3Adapter(
+        "AKIAIJRIGNXA2BOCWIGA",
+        "IfTGFjY2XtAKe8mopz7Bbfk30YtlF8cnlXGhD1ub",
+        "wnd-files-test",
+        {directAccess: true}
+    ),
+    liveQuery: {
+        classNames: ["Posts", "Comments", "Conversation", "Message", "Fcm"] // List of classes to support for query subscriptions
+    }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -78,19 +81,19 @@ app.use(mountPath, api);
 //app.use();
 
 // Parse Server plays nicely with the rest of your web routes
-app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+app.get('/', function (req, res) {
+    res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+app.get('/test', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
-httpServer.listen(port, function() {
+httpServer.listen(port, function () {
     console.log('parse-server-example running on port ' + port + '.');
 });
 
